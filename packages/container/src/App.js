387b8fd1267e65b994ee4042a572cd3cef02fa8a
Guilherme Nass <nassguilherme@gmail.com>
2023-12-04 07:47:35 -0300

    fix import

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MarketingApp from '../components/MarketingApp'
 import Header from '../components/Header'
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
+import { BrowserRouter } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 const generateClassName = createGenerateClassName({
